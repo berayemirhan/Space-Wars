410b40ef58ad65a74338fc96e32d4ce62f76289a
berayemirhan <94457263+berayemirhan@users.noreply.github.com>
2021-12-13 13:52:29 +0300

    13.12.2021

---
+++ Space Wars/Assets/Scripts/Directions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Directions: MonoBehaviour
+{ 
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void quit()
+    {
+        Application.Quit();
+        
+    }
+    public void mainmenü()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
