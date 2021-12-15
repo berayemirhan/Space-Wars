edbeed3c389777cb011f7d08680be2eb2aff6d5c
berayemirhan <94457263+berayemirhan@users.noreply.github.com>
2021-12-15 16:06:29 +0300

    15.12.2021

---
+++ Space Wars/Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public Transform[] SpawnPoınts;
+    public GameObject[] EnemyPrefebs;
+    public float sayac;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        sayac -= Time.deltaTime;
+        if (sayac<=0)
+        {
+            sayac = 3f;
+            int RandEnemy = Random.Range(0, EnemyPrefebs.Length);
+            int RandSpawnPoint = Random.Range(0, SpawnPoınts.Length);
+
+            Instantiate(EnemyPrefebs[0], SpawnPoınts[RandSpawnPoint].position, transform.rotation);
+
+        }
+    }
+}
---
+++ Space Wars/Assets/collision2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision2)
+    {
+        if(collision2.gameObject.tag=="enemys")
+        {
+            Destroy(collision2.gameObject);
+            
+        }
+    }
+}
